f092e0243b41b3b578b501bcee0e36871aea7a96
ANNALIISA_LUIK <alluik@ymail.com>
2023-09-06 13:42:42 +0300

    Initial commit

---
+++ src/main/java/com/example/rainaiback/RainaibackApplication.java
@@ -0,0 +1,13 @@
+package com.example.rainaiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainaibackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainaibackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rainaiback/RainaibackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rainaiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainaibackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
