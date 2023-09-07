7d243c2b4eb1791a5efbf2c99710b284f39dad7d
ANNALIISA_LUIK <alluik@ymail.com>
2023-09-07 09:31:09 +0300

    signup get complete + lisasin apidokumentatsiooni ja apierrori vist ka

--- src/main/java/ee/valiit/rainaiback/business/SignupService.java
+++ src/main/java/ee/valiit/rainaiback/business/SignupService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class SignUpService {
+public class SignupService {
 
     @Resource
     private PackageTypeService packageTypeService;
